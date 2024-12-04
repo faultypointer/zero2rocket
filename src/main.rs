28e46895370aaaa803ead28fdd71cfb760e10a2c
faultypointer <faultypointer@proton.me>
2024-12-04 20:14:15 +0545

    health check endpoint with rocket

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use rocket::http::Status;
+
+#[macro_use]
+extern crate rocket;
+
+#[get("/health_check")]
+fn health_check() -> Status {
+    Status::Ok
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![health_check])
 }
